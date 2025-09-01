70eab28ac5716015eb356e80ef39f709ace5e751
krishnayadav9793 <karanyadav21398@gmail.com>
2025-09-01 12:36:57 +0530

    server

---
+++ backend/server.java
@@ -0,0 +1,21 @@
+package backend;
+import java.util.*;
+class player{
+    protected String name ;
+    protected int[] highScores;
+}
+class multiPlayer{
+    protected int numberOfPlayer;
+    protected player p1; 
+}
+class singlePlayer {
+    protected player p;
+}
+class Game2048 extends singlePlayer{
+
+}
+public class server {
+    public static void main(String[] args) {
+        
+    }
+}
